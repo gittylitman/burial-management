b9177fc2517756c6a00f91b1c2b548f84debce23
Y. Charaz <yeuditc@skyvar.co.il>
2024-12-09 11:10:05 +0200

    :hammer: Add connection to gov and filter cemetery by burial city

---
+++ app/services/ConnectGov.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class ConnectGov
+{
+    public function connectGov()
+    {
+        $url = config('services.gov.url');
+        $response = Http::get($url);
+        $response = $response->json();
+
+        return collect($response['result']['records']);
+    }
+
+    public function getCemeteryCities()
+    {
+        $filtered_names = $this->connectGov()
+            ->filter(function ($item) {
+                return isset($item['SETL_NAME']) && $item['SETL_NAME'] !== null;
+            })
+            ->map(function ($item) {
+                return [$item['SETL_NAME'] => $item['SETL_NAME']];
+            })
+            ->unique()
+            ->values()
+            ->collapse()
+            ->all();
+
+        return $filtered_names;
+    }
+
+    public function getCemeteryByCity($city)
+    {
+        $filtered_names = $this->connectGov()
+            ->filter(function ($record) use ($city) {
+                return $record['SETL_NAME'] === $city;
+            })
+            ->map(function ($item) {
+                return [$item['NAME'] => $item['NAME']];
+            })
+            ->values()
+            ->collapse()
+            ->all();
+
+        return $filtered_names;
+    }
+}
