00dfa3cf308b580ee17c589d924e39b3b3087c79
Y. Charaz <yeuditc@skyvar.co.il>
2024-12-15 16:00:39 +0200

    aaa

--- graves/resources/views/tables/columns/cemetery-url.blade.php
+++ graves/resources/views/tables/columns/cemetery-url.blade.php
@@ -1,8 +1,3 @@
-
-@php
-$url = config('services.gov.url_maps').session('burial_city').",".session('cemetery') ;
-@endphp
-
 <div>
-    <a href="{{ $url }}" target="_blank">לחץ כאן</a>
+    <a href="{{ config('services.gov.url_maps') }}{{$getRecord()->burial_city}},{{$getRecord()->grave->cemetery}}" target="_blank">לחץ כאן</a>
 </div>
